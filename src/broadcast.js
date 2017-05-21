b7abc362552a07ae8d92cf35535dc14518e10037
Kai Curry <kai@cape.io>
2017-05-21 15:54:53 -0400

    remove broadcast logs

--- src/broadcast.js
+++ src/broadcast.js
@@ -9,6 +9,6 @@ export default function sendMsg(message, host, port) {
   const data = new Buffer(message)
   socket.send(data, 0, data.length, port, host, (err) => {
     if (err) throw err
-    console.log(message, host, port)
+    // console.log(message, host, port)
   })
 }
