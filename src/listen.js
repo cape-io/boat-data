94f6ba2178b1179c2c41341120ac8dd48bfe6e34
Kai Curry <kai@cape.io>
2017-08-31 23:12:20 -0400

    Listen for specific value instead of obj.

--- src/listen.js
+++ src/listen.js
@@ -1,6 +1,7 @@
 import { get } from 'lodash/fp'
 import { InfluxDB } from 'influx'
 import { addListener } from 'cape-redux'
+import { select } from 'cape-select'
 import sendMsg from './broadcast'
 import { dbt, mvw } from './nmea/encode'
 
@@ -19,13 +20,14 @@ function sendDepth(reduxStore, meters) {
   influx.writePoints([{ measurement: 'depth', fields: { value: meters } }])
   // console.log('depth', meters)
 }
-const getWindSpeed = get('data.data.115.130306.fields')
-function sendWind(reduxStore, fields) {
+const getWind = get('data.data.115.130306.fields')
+const getWindSpeed = select(getWind, 'Wind Speed')
+const getWindAngle = select(getWind, 'Wind Angle')
+function sendWind(reduxStore, speed) {
   const state = reduxStore.getState()
-  const speed = fields['Wind Speed']
-  const angle = fields['Wind Angle']
+  const angle = getWindAngle(state)
   const sentence = mvw({ angle, reference: 'R', speed, unit: 'M' })
-  console.log('mvw', sentence)
+  // console.log('mvw', sentence)
   sendMsg(sentence, state.config.lanBroadcast, state.config.navionicsPort)
   influx.writePoints([{ measurement: 'windSpeed', fields: { value: speed } }])
 }
---
+++ src/udpListen.js
@@ -0,0 +1,15 @@
+import dgram from 'dgram'
+
+const socket = dgram.createSocket('udp4')
+
+socket.on('listening', () => {
+  const address = socket.address()
+  console.log(`UDP Server listening on ${address.address}:${address.port}`)
+})
+
+socket.on('message', (message) => {
+  // console.log(`${remote.address}:${remote.port} - ${message}`)
+  console.log(message)
+  // process.stdout.write(message)
+})
+socket.bind(10110)
