88c6e7c7995280cd1e182b32546fe40d52d3672a
Kai Curry <kai@cape.io>
2017-09-14 19:41:51 -0400

    try serial fix

--- src/serial/index.js
+++ src/serial/index.js
@@ -1,18 +1,17 @@
 import SerialPort from 'serialport'
 import { serialClose, serialData, serialErr, serialOpen } from './actions'
 
-const Readline = SerialPort.parsers.Readline
-const parser = new Readline()
-
 export default function initSerial(dispatcher, options) {
+  const Readline = SerialPort.parsers.Readline
+  const parser = new Readline()
   const { baudRate, devicePath } = options
   console.log('init Serial', devicePath, baudRate)
   const serial = new SerialPort(devicePath, {
     baudRate,
   })
   serial.pipe(parser)
-  serial.on('open', dispatcher(serialOpen))
-  serial.on('error', dispatcher(serialErr))
-  serial.on('close', dispatcher(serialClose))
-  serial.on('data', dispatcher(serialData))
+  parser.on('open', dispatcher(serialOpen))
+  parser.on('error', dispatcher(serialErr))
+  parser.on('close', dispatcher(serialClose))
+  parser.on('data', dispatcher(serialData))
 }
