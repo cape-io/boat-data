d8d41dfaa0721153862db2fab43f7babf41a12ab
Kai Curry <kai@cape.io>
2017-10-09 22:01:07 -0400

    rm persist

--- src/createStore.js
+++ src/createStore.js
@@ -1,25 +1,15 @@
 import { applyMiddleware, createStore } from 'redux'
-import { persistStore, persistReducer } from 'redux-persist'
-import { AsyncNodeStorage } from 'redux-persist-node-storage'
+// import { persistStore, persistReducer } from 'redux-persist'
+// import { AsyncNodeStorage } from 'redux-persist-node-storage'
 
 import reducer from './reducer'
 import middleware from './middleware'
 
-const storage = new AsyncNodeStorage('/tmp/redux')
-const config = {
-  key: 'boatData', // key is required
-  storage, // storage is now required
-  throttle: 90000,
-}
-
 export default function configureStore(initState) {
   const store = createStore(
-    persistReducer(config, reducer),
+    reducer,
     initState,
     applyMiddleware(middleware)
   )
-  return {
-    store,
-    persistor: persistStore(store),
-  }
+  return store
 }
