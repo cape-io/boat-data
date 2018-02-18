8449edc1fd794c3e5e3a19a37dd96423a610b173
Kai Curry <kai@cape.io>
2018-02-18 17:03:55 -0500

    action cleanup

--- src/serial/actions.js
+++ src/serial/actions.js
@@ -1,4 +1,4 @@
-import { toString } from 'lodash'
+import { overEvery, toString } from 'lodash/fp'
 import { createSimpleAction } from 'cape-redux'
 import { getName, isAis, isData } from '../nmea/decode'
 
@@ -12,28 +12,30 @@ export function createDataPayload(payload) {
 }
 // export const NMEA_AIS = 'boatData/NmeaAIS'
 // export const nmeaAis = createSimpleAction(NMEA_AIS, createDataPayload)
-
-// export const NMEA_DATA = 'boatData/NmeaData'
-export const SERIAL_DATA = 'boatData/SerialData'
-export const serialData = createSimpleAction(SERIAL_DATA, createDataPayload)
-
 // export const nmeaData = createSimpleAction(NMEA_DATA, createDataPayload)
 // export const serialData = cond([
 //   [isAis, nmeaAis],
 //   [isData, nmeaData],
 // ])
 
-export const SERIAL_CLOSE = 'boatData/SerialClose'
+export const SERIAL_DATA = 'serial/SerialData'
+export const serialData = createSimpleAction(SERIAL_DATA, createDataPayload)
+
+export const SERIAL_CLOSE = 'serial/SerialClose'
 export function serialClose(payload) {
   console.error(`Serial process exited with code ${payload}.`)
   return { type: SERIAL_CLOSE, payload }
 }
 
-export const SERIAL_ERROR = 'boatData/SerialError'
-export const serialErr = createSimpleAction(SERIAL_ERROR, toString)
+export const SERIAL_ERROR = 'serial/SerialError'
+export const serialErr = overEvery([
+  console.error,
+  createSimpleAction(SERIAL_ERROR, toString),
+])
 
-export const SERIAL_OPEN = 'boatData/SerialOpen'
+
+export const SERIAL_OPEN = 'serial/SerialOpen'
 export function serialOpen() {
-  console.log('serial port open')
+  console.log('Serial port open')
   return { type: SERIAL_OPEN }
 }
