12fa61438568c3c6bf0bbd711fb794bad7538ac4
Kai Curry <kai@cape.io>
2018-02-18 20:05:04 -0500

    handle close on serial port

--- src/serial/index.js
+++ src/serial/index.js
@@ -1,22 +1,23 @@
 import SerialPort from 'serialport'
-import { overEvery } from 'lodash/fp'
 import { serialClose, serialData, serialErr, serialOpen } from './actions'
 
 export default function initSerial(dispatcher, options) {
   const Readline = SerialPort.parsers.Readline
-  const parser = new Readline()
   const { baudRate, devicePath } = options
   console.log('init Serial', devicePath, baudRate)
   const serial = new SerialPort(devicePath, {
     baudRate,
   })
-  const handleError = overEvery([console.error, dispatcher(serialErr)])
-  // handleError('testing')
+  const handleError = dispatcher(serialErr)
+  const handleClose = dispatcher(serialClose)
   // Open errors will be emitted as an error event
   serial.on('error', handleError)
+  serial.on('close', handleClose)
+  const parser = new Readline()
   serial.pipe(parser)
   parser.on('open', dispatcher(serialOpen))
   parser.on('error', handleError)
-  parser.on('close', dispatcher(serialClose))
+  parser.on('close', handleClose)
   parser.on('data', dispatcher(serialData))
+  console.log('Serial is open?', serial.isOpen)
 }
