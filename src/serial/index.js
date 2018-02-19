e17970b91bea3d15cb465f3815aa7b1a816ef4bc
Kai Curry <kai@cape.io>
2018-02-19 16:49:46 -0500

    try to restart on close

--- src/serial/index.js
+++ src/serial/index.js
@@ -4,26 +4,36 @@ import { serialClose, serialData, serialErr, serialOpen } from './actions'
 export default function initSerial(dispatcher, options) {
   const { baudRate, devicePath } = options
   console.log('init Serial', devicePath, baudRate)
-  const serial = new SerialPort(devicePath, {
-    baudRate,
-  })
   const handleError = dispatcher(serialErr)
   const handleClose = dispatcher(serialClose)
   const handleOpen = dispatcher(serialOpen)
-
-  function onClose(err) {
-    handleClose(err)
-    serial.open(handleOpen)
-    console.log('Serial is open?', serial.isOpen)
-  }
-  // Open errors will be emitted as an error event
-  serial.on('error', handleError)
-  serial.on('close', onClose)
   const parser = new SerialPort.parsers.Readline()
-  serial.pipe(parser)
+
+  let serial = null
+  function cleanup() {
+    if (serial === null) return
+    serial.unpipe(parser)
+    serial.removeAllListeners()
+    serial = null
+  }
+  function start() {
+    cleanup()
+    serial = new SerialPort(devicePath, {
+      baudRate,
+    })
+    function onClose(err) {
+      handleClose(err)
+      start()
+    }
+    serial.on('error', handleError)
+    serial.on('close', onClose)
+    serial.pipe(parser)
+  }
+
   parser.on('open', handleOpen)
   parser.on('error', handleError)
   parser.on('close', handleClose)
   parser.on('data', dispatcher(serialData))
-  console.log('Serial is open?', serial.isOpen)
+  start()
+  // console.log('Serial is open?', serial.isOpen)
 }
