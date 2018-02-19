37b6293803f8308746f0c01bb009c18e5fcb1717
Kai Curry <kai@cape.io>
2018-02-19 16:32:57 -0500

    try to reopen basic

--- src/serial/index.js
+++ src/serial/index.js
@@ -2,7 +2,6 @@ import SerialPort from 'serialport'
 import { serialClose, serialData, serialErr, serialOpen } from './actions'
 
 export default function initSerial(dispatcher, options) {
-  const Readline = SerialPort.parsers.Readline
   const { baudRate, devicePath } = options
   console.log('init Serial', devicePath, baudRate)
   const serial = new SerialPort(devicePath, {
@@ -10,12 +9,19 @@ export default function initSerial(dispatcher, options) {
   })
   const handleError = dispatcher(serialErr)
   const handleClose = dispatcher(serialClose)
+  const handleOpen = dispatcher(serialOpen)
+
+  function onClose(err) {
+    handleClose(err)
+    serial.open(handleOpen)
+    console.log('Serial is open?', serial.isOpen)
+  }
   // Open errors will be emitted as an error event
   serial.on('error', handleError)
-  serial.on('close', handleClose)
-  const parser = new Readline()
+  serial.on('close', onClose)
+  const parser = new SerialPort.parsers.Readline()
   serial.pipe(parser)
-  parser.on('open', dispatcher(serialOpen))
+  parser.on('open', handleOpen)
   parser.on('error', handleError)
   parser.on('close', handleClose)
   parser.on('data', dispatcher(serialData))
